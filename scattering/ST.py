2891992df6b3debaee0a7e6aa411b8055b59fbfc
Sihao Cheng <s.cheng@jhu.edu>
2022-07-03 20:28:34 +0200

    Update ST.py

--- scattering/ST.py
+++ scattering/ST.py
@@ -1538,9 +1538,9 @@ class Bispectrum_Calculator(object):
         self.device = device
         if k_range is None:
             if bin_type=='linear':
-                k_range = np.linspace(1, M/2*1.4, bins) # linear binning
+                k_range = np.linspace(1, M/2*1.4, bins+1) # linear binning
             if bin_type=='log':
-                k_range = np.logspace(0, np.log10(M/2*1.4), bins) # log binning
+                k_range = np.logspace(0, np.log10(M/2*1.4), bins+1) # log binning
         self.k_range = k_range
         self.M = M
         self.N = N
@@ -1572,7 +1572,10 @@ class Bispectrum_Calculator(object):
             self.select = self.select.cuda()
             self.B_ref_array = self.B_ref_array.cuda()
     
-    def forward(self, image):
+    def forward(self, image, normalization='image'):
+        '''
+        normalization is either 'image' or 'dirac'.
+        '''
         if type(image) == np.ndarray:
             image = torch.from_numpy(image)
 
@@ -1586,10 +1589,16 @@ class Bispectrum_Calculator(object):
             B_array = B_array.cuda()
         
         image_f = torch.fft.fftn(image, dim=(-2,-1))
-        conv = torch.fft.ifftn(
-            image_f[None,...] * self.k_filters_torch[:,None,...],
-            dim=(-2,-1)
-        ).real
+        if normalization=='image':
+            conv = torch.fft.ifftn(
+                image_f[None,...] * self.k_filters_torch[:,None,...],
+                dim=(-2,-1)
+            ).real
+        if normalization=='dirac':
+            conv = torch.fft.ifftn(
+                image_f[None,...]*0 + self.k_filters_torch[:,None,...],
+                dim=(-2,-1)
+            ).real
         conv_std = conv.std((-1,-2))
         for i1 in range(len(self.k_range)-1):
             for i2 in range(i1+1):
@@ -1601,7 +1610,7 @@ class Bispectrum_Calculator(object):
                         # *1e8 # / self.B_ref_array[k1, k2, k3]
         return B_array.reshape(len(image), (len(self.k_range)-1)**3)[:,self.select.flatten()]
 
-def get_power_spectrum(image, bins, bin_type='log', device='gpu'):
+def get_power_spectrum(image, k_range=None, bins=None, bin_type='log', device='gpu'):
     '''
     get the power spectrum of a given image
     '''
@@ -1622,22 +1631,24 @@ def get_power_spectrum(image, bins, bin_type='log', device='gpu'):
     X = torch.arange(0,M)
     Y = torch.arange(0,N)
     Ygrid, Xgrid = torch.meshgrid(Y,X, indexing='ij')
-    R = ((Xgrid - M/2)**2 + (Ygrid - N/2)**2)**0.5
-    if bin_type=='log': 
-        R_range = torch.logspace(0.0, np.log10(1.4*M/2), bins+1)
-    else:
-        R_range = torch.linspace(1, 1.4*M/2, bins+1)
-#     R_range = torch.cat((torch.tensor([0]), R_range))
-    power_spectrum = torch.zeros(len(image), len(R_range)-1, dtype=image.dtype)
+    k = ((Xgrid - M/2)**2 + (Ygrid - N/2)**2)**0.5
+    
+    if k_range is None:
+        if bin_type=='linear':
+            k_range = torch.linspace(1, M/2*1.4, bins+1) # linear binning
+        if bin_type=='log':
+            k_range = torch.logspace(0, np.log10(M/2*1.4), bins+1) # log binning
+
+    power_spectrum = torch.zeros(len(image), len(k_range)-1, dtype=image.dtype)
     if device=='gpu':
-        R = R.cuda()
-        R_range = R_range.cuda()
+        k = k.cuda()
+        k_range = k_range.cuda()
         power_spectrum = power_spectrum.cuda()
 
-    for i in range(len(R_range)-1):
-        select = (R >= R_range[i]) * (R < R_range[i+1])
+    for i in range(len(k_range)-1):
+        select = (k >= k_range[i]) * (k < k_range[i+1])
         power_spectrum[:,i] = ((modulus**2*select[None,...]).sum((-2,-1))/select.sum()).log()
-    return power_spectrum, R_range
+    return power_spectrum, k_range
 
 def reduced_ST(S, J, L):
     s0 = S[:,0:1]
