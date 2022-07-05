5fe7f3f5ef2d52ee6de9a70dff7b418519ff0a77
Sihao Cheng <s.cheng@jhu.edu>
2022-07-05 23:20:43 +0200

    Update __init__.py

--- scattering/__init__.py
+++ scattering/__init__.py
@@ -213,7 +213,7 @@ def func_hist_j(image, J):
         flat_image.sort(dim=-1).values.reshape(len(image),-1,image.shape[-2]).mean(-1) / flat_image.std(-1)[:,None]
     )
     for j in range(J):
-        smoothed_image = smooth(image, j)[:,::2**j,::2**j]
+        smoothed_image = smooth(image, j)#[:,::2**j,::2**j]
         flat_image = smoothed_image.reshape(len(image),-1)
         cumsum_list.append(
             flat_image.sort(dim=-1).values.reshape(len(image),-1,smoothed_image.shape[-2]).mean(-1) / flat_image.std(-1)[:,None]
