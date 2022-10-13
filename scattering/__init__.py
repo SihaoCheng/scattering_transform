9399f0b317d96c7e37803b9ce9f84ce5e5e99d9a
Sihao Cheng <scheng@ias.edu>
2022-10-13 14:23:56 -0400

    Update __init__.py

--- scattering/__init__.py
+++ scattering/__init__.py
@@ -173,7 +173,7 @@ Use * or + to connect more than one condition.
         ps_bins = J-1
     def func_ps(image):
         ps, _ = get_power_spectrum(image, bins=ps_bins, bin_type=ps_bin_type)
-        return torch.cat(((image.mean((-2,-1))/image.std((-2,-1)))[:,None], image.var((-2,-1)))[:,None], ps), axis=-1)
+        return torch.cat(( (image.mean((-2,-1))/image.std((-2,-1)))[:,None], image.var((-2,-1))[:,None], ps), axis=-1)
     # bispectrum
     if bi:
         if bispectrum_bins is None:
