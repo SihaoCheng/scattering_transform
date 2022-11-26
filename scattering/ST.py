13aa996c80fba12e5b0ba698da5d75b6a312ba03
Sihao Cheng <scheng@ias.edu>
2022-11-26 17:26:43 -0500

    Update ST.py

--- scattering/ST.py
+++ scattering/ST.py
@@ -613,7 +613,7 @@ class Scattering2d(object):
             torch.arange(J), torch.arange(J), 
             torch.arange(L), torch.arange(L), indexing='ij'
         )
-        select_2 = j1 < j2
+        select_2 = j1 <= j2
         invalid = j1[None,select_2]*0-1
         index_2 = torch.cat(
             (j1[None,select_2], j2[None,select_2], invalid, 
@@ -621,7 +621,7 @@ class Scattering2d(object):
             dim=0)
         # two-scale isotropic coef
         j1, j2, l2 = torch.meshgrid(torch.arange(J), torch.arange(J), torch.arange(L), indexing='ij')
-        select_2_iso = j1 < j2
+        select_2_iso = j1 <= j2
         invalid = j1[None,select_2_iso]*0-1
         index_2_iso = torch.cat(
             (j1[None,select_2_iso], j2[None,select_2_iso], invalid, 
@@ -633,9 +633,9 @@ class Scattering2d(object):
             torch.arange(L), torch.arange(L), torch.arange(L), indexing='ij'
         )
         if normalization=='P00' and num_field==1:
-            select_3 = (j1 <= j2) * (j2 < j3) * eval(C11_criteria)
+            select_3 = (j1 <= j2) * (j2 <= j3) * eval(C11_criteria)
         else:
-            select_3 = (j1 <= j2) * (j2 < j3) * eval(C11_criteria) * ~((l1==l2)*(j1==j2))
+            select_3 = (j1 <= j2) * (j2 <= j3) * eval(C11_criteria) * ~((l1==l2)*(j1==j2))
         index_3 = torch.cat(
             (j1[None,select_3], j2[None,select_3], j3[None,select_3],
              l1[None,select_3], l2[None,select_3], l3[None,select_3]),
@@ -646,9 +646,9 @@ class Scattering2d(object):
             torch.arange(L), torch.arange(L), indexing='ij'
         )
         if normalization=='P00' and num_field==1:
-            select_3_iso = (j1 <= j2) * (j2 < j3) * eval(C11_criteria)
+            select_3_iso = (j1 <= j2) * (j2 <= j3) * eval(C11_criteria)
         else:
-            select_3_iso = (j1 <= j2) * (j2 < j3) * eval(C11_criteria) * ~((l2==0)*(j1==j2))
+            select_3_iso = (j1 <= j2) * (j2 <= j3) * eval(C11_criteria) * ~((l2==0)*(j1==j2))
         invalid = j1[None,select_3_iso]*0-1
         index_3_iso = torch.cat(
             (j1[None,select_3_iso], j2[None,select_3_iso], j3[None,select_3_iso],
